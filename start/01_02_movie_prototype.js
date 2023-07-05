cac0fd849f8a97c0f5bdde912be2a1df2d027b52
Kevon Coley <114433474+kcoley1994@users.noreply.github.com>
2023-07-05 01:40:51 +0000

    javaScript LinkedIn Learning first commit

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,16 @@
-// Write your code here
+class Movies {
+  constructor(title, director, genre, releaseYear, rating){
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  getOverview(){
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+  }
+}
+
+const BlackPanther = new Movies('Black Panther', 'Ryan Coogler', 'Action', 2018, 5 )
+console.log(BlackPanther.getOverview())
