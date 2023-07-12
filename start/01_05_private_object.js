9d1036a18ae017bb8f20300e794c6e1740cd0dfb
Kevon Coley <114433474+kcoley1994@users.noreply.github.com>
2023-07-12 17:31:10 +0000

    second commit

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,11 @@
-// Write your code here
+const username = Symbol('username');
+const password = Symbol('password');
+
+const user = {
+  [username]: 'kcoley1',
+  password: '1234565',
+  age: 29
+};
+
+console.log(user.username);
+console.log(user.password);
--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,22 @@
 // Write your code here
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssistant, students;
+
+  if (hasTeachingAssistant){
+    [teacher, teachingAssistant, ...students] =classList;
+  } else {
+    [teacher, ...students] = classList
+  }
+  return students;
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: true, 
+  classList:["Rashida", "John", "Roman", "Lisa", "Omar", "Luke"]
+}));
+
+console.log(getStudents({
+  hasTeachingAssistant: false, 
+  classList:["Rashida", "John", "Roman", "Lisa", "Omar", "Luke"]
+}));
